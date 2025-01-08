c9d9292bc9623908ff24546b22aeaf80b09eaee8
GalAngel15 <galangel151@gmail.com>
2025-01-08 11:43:43 +0200

    Initial commit

---
+++ src/test/java/com/example/stocks/stocksapi/StocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocks.stocksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
