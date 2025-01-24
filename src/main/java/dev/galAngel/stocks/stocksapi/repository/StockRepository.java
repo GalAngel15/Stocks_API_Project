6d733460c3e163583a6b757321852bbaab2648a6
GalAngel15 <galangel151@gmail.com>
2025-01-25 01:13:55 +0200

    Fixing bugs

--- src/main/java/dev/galAngel/stocks/stocksapi/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.galAngel.stocks.stocksapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface StockRepository extends MongoRepository<StockEntity, String> {
-}
